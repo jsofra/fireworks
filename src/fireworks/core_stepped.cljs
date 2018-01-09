d61481072839ba4cc14f419242a887faf0a57c1c
James Sofra <james.sofra@gmail.com>
2018-01-09 20:14:00 +1100

    4. define stage

--- src/fireworks/core_stepped.cljs
+++ src/fireworks/core_stepped.cljs
@@ -9,3 +9,8 @@
   {:pixi.renderer/size             [canvas-size canvas-size]
    :pixi.renderer/background-color 0x0a1c5e
    :pixi.renderer/transparent?     false})
+
+(def stage
+  {:impi/key                :stage
+   :pixi.object/type        :pixi.object.type/container
+   :pixi.container/children []})
