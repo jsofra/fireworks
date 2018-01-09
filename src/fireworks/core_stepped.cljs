5d4dc39876151a4334039090980e457459dbef6c
James Sofra <james.sofra@gmail.com>
2018-01-09 20:10:18 +1100

    3. define canvas

--- src/fireworks/core_stepped.cljs
+++ src/fireworks/core_stepped.cljs
@@ -2,3 +2,10 @@
   (:require [impi.core :as impi]))
 
 (enable-console-print!)
+
+(def canvas-size 600)
+
+(def canvas
+  {:pixi.renderer/size             [canvas-size canvas-size]
+   :pixi.renderer/background-color 0x0a1c5e
+   :pixi.renderer/transparent?     false})
