0ffb7db7cd2697933041afa1875e99f1c4fec85a
James Sofra <james.sofra@gmail.com>
2018-01-09 23:30:28 +1100

    13. Add firework

--- src/fireworks/core_stepped.cljs
+++ src/fireworks/core_stepped.cljs
@@ -20,24 +20,33 @@
    :pixi.sprite/anchor   [0.5 0.5]
    :pixi.sprite/texture  {:pixi.texture/source "img/clj-melb-logo.png"}})
 
-(defn spark! [pos]
-  (let [tint (* (Math/random) 0xFFFFFF)]
-    {:impi/key             "spark"
-     :pixi.object/type     :pixi.object.type/sprite
-     :pixi.object/position pos
-     :pixi.sprite/anchor   [0.5 0.5]
-     :pixi.sprite/tint     tint
-     :pixi.sprite/texture  {:pixi.texture/source "img/spark.png"}}))
+(defn spark [id pos tint]
+  {:impi/key             id
+   :pixi.object/type     :pixi.object.type/sprite
+   :pixi.object/position pos
+   :pixi.sprite/anchor   [0.5 0.5]
+   :pixi.sprite/tint     tint
+   :pixi.sprite/texture  {:pixi.texture/source "img/spark.png"}})
 
-(defn direction [fraction]
-  [(Math/cos (* 2 Math/PI fraction))
-   (Math/sin (* 2 Math/PI fraction))])
+(defn velocity [speed n-sparks nth-spark]
+  [(* speed (Math/cos (* 2 Math/PI (/ nth-spark n-sparks))))
+   (* speed (Math/sin (* 2 Math/PI (/ nth-spark n-sparks))))])
+
+(defn firework [id n-sparks pos speed tint]
+  {:impi/key                (keyword (str "firework_" id))
+   :firework/id             id
+   :pixi.object/type        :pixi.object.type/container
+   :pixi.container/children (mapv #(spark (str "firework_" id "_particle_" %)
+                                          (mapv + pos (velocity speed n-sparks %))
+                                          tint)
+                                  (range n-sparks))})
 
 (def stage
   {:impi/key                :stage
    :pixi.object/type        :pixi.object.type/container
    :pixi.container/children [clj-melb-logo
-                             (spark! [300 150])]})
+                             (let [tint (* (Math/random) 0xFFFFFF)]
+                               (firework "id" 8 [300 150] 50 tint))]})
 
 (def state
   {:pixi/renderer canvas
